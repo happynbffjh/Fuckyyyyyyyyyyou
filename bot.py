0ccd8fdf90665d820d63ac5e3ef6ba05b6adc8fd
happynbffjh <happybroyo19@gmail.com>
2026-03-15 14:58:47 +0600

    Update print statement from 'Hello' to 'Goodbye'

--- bot.py
+++ bot.py
@@ -28,8 +28,7 @@ import nest_asyncio
 nest_asyncio.apply()
 
 # Configuration
-BOT_TOKEN = "8783552179:AAGZtUgr4AEONHyuNPPZ07na09ZHFwK3EjA"
-API_ID = 23933044
+BOT_TOKEN = "8435065448:AAF3deY52T_TRETXKPgZnqOaqyfHXzUVlZ4"
 API_HASH = "6df11147cbec7d62a323f0f498c8c03a"
 ADMINS = [7125341830]
 MONGO_URL = "mongodb+srv://animepahe:animepahe@animepahe.o8zgy.mongodb.net/?retryWrites=true&w=majority"
